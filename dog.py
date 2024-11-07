731fc722d51ebff027392d9ded0f9930df0b50df
Thomas Rugeles <thomas.rugeles@ucas-edu.net>
2024-11-06 19:18:39 -0700

    Update dog.py

--- dog.py
+++ dog.py
@@ -1,17 +1,21 @@
 from csv import reader
 
 class Dog:
-    def __init__(self, name, breed):
+    def __init__(self, number, name, breed):
         self.name = name
         self.breed = breed
+        self.number = number
+        
+    def __str__(self):
+        return f"Number: {self.number}, Name: {self.name}, Breed: {self.breed}."
 
 dogs = []
 
 with open('dogs.csv') as csv_file:
     csv_reader = reader(csv_file, delimiter=',')
     next(csv_reader)
-    for name, breed in csv_reader:
-        dogs.append(Dog(name, breed))
+    for number, name, breed in csv_reader:
+        dogs.append(Dog(number, name, breed))
 
 for dog in dogs:
     print(dog)
